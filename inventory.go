683ffba0c03fbab1033b5f5474364b69d7969f52
Remi Philippe <rephilip@cisco.com>
2018-03-29 11:41:41 -0700

    added license

--- inventory.go
+++ inventory.go
@@ -9,6 +9,8 @@ import (
 )
 
 // Inventory Inventory structure holding a single inventory item
+// tags_scope_name can be a string
+// E0329 11:34:51.834252   10294 inventory.go:46] Error unmarshalling json json: cannot unmarshal string into Go struct field Inventory.tags_scope_name of type []string / content: {"results":[{"address_type":"IPV4","host_name":"app01","host_uuid":"f22e3c96884084b5d4334ba977e9ca8a7564c1f1","iface_mac":"fa:16:3e:58:84:24","iface_name":"eth0","ip":"192.168.17.151","netmask":"255.255.255.0","os":"CentOS","os_version":"7.4","tags_is_internal":"true","tags_scope_name":"Default","user_aci-fab":null,"user_anp":null,"user_des":null,"user_desc":null,"user_dns":null,"user_epg":null,"user_pod":null,"user_subnet":null,"user_support-group":"cips_group","user_tenant":null,"user_type":null,"user_vrf":null,"user_zone":null,"uuid_src":"SENSOR","vrf_id":"1","vrf_name":"Default"}]}
 type Inventory struct {
 	Hostname    string   `json:"host_name"`
 	AddressType string   `json:"address_type"`
@@ -43,7 +45,7 @@ func (h *H4) InventorySearch(s *InventoryQuery) ([]Inventory, error) {
 	jsonResp := make(map[string][]Inventory)
 	err = json.Unmarshal(postResp, &jsonResp)
 	if err != nil {
-		glog.Errorf("Error unmarshalling json %s", err)
+		glog.Errorf("Error unmarshalling json %s / content: %s", err, postResp)
 		return nil, err
 	}
 
